a8c91d1783ae9346faf4093905c3aa1438af4419
wontknow <zdb.ytg@gmail.com>
2025-09-29 21:26:51 +0200

    updated models to be case conform and differ between API models and DB schema

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,4 +1,6 @@
 from .customer import Customer  # noqa: F401
 from .invoice import Invoice  # noqa: F401
-from .invoice_item import Invoice_Item  # noqa: F401
+from .invoice_item import InvoiceItem  # noqa: F401
 from .profile import Profile  # noqa: F401
+from .invoice_create import InvoiceCreate  # noqa: F401
+from .invoice_read import InvoiceRead, InvoiceItemRead  # noqa: F401
---
+++ backend/models/invoice_read.py
@@ -0,0 +1,20 @@
+from typing import List
+from sqlmodel import SQLModel
+
+
+class InvoiceItemRead(SQLModel):
+    id: int
+    invoice_id: int
+    quantity: int
+    description: str
+    price: float
+
+
+class InvoiceRead(SQLModel):
+    id: int
+    number: str
+    date: str
+    customer_id: int
+    profile_id: int
+    total_amount: float
+    invoice_items: List[InvoiceItemRead] = []
