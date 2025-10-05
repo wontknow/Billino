eb7ed7d6469c4932eebd61e109eb1ac81ef34a96
wontknow <zdb.ytg@gmail.com>
2025-10-05 20:40:43 +0200

    added city to profile model

--- backend/models/profile.py
+++ backend/models/profile.py
@@ -7,5 +7,6 @@ class Profile(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     address: str
+    city: str
     bank_data: Optional[str] = None
     tax_number: Optional[str] = None
