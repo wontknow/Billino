3efdd1834cafeebd9ebbbdaa05dd61487642ba07
wontknow <zdb.ytg@gmail.com>
2025-10-06 22:36:35 +0200

    added include_tax tax_rate is_gross_amount to invoice read

--- backend/models/invoice_read.py
+++ backend/models/invoice_read.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from sqlmodel import SQLModel
 
@@ -17,6 +17,9 @@ class InvoiceRead(SQLModel):
     date: str
     customer_id: int
     profile_id: int
-    include_tax: bool
     total_amount: float
     invoice_items: List[InvoiceItemRead] = []
+    include_tax: Optional[bool] = None
+    tax_rate: Optional[float] = None
+    is_gross_amount: bool = True
+
