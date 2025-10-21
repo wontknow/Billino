77fbd17cefbcb5cbda499ad27c452e061fa4232b
wontknow <zdb.ytg@gmail.com>
2025-10-21 16:45:20 +0200

    Datenmodel

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -4,6 +4,7 @@ from .invoice_create import InvoiceCreate, InvoiceCreateWithNumber  # noqa: F401
 from .invoice_item import InvoiceItem  # noqa: F401
 from .invoice_read import InvoiceItemRead, InvoiceRead  # noqa: F401
 from .profile import Profile  # noqa: F401
+from .stored_pdf import StoredPDF, StoredPDFCreate, StoredPDFRead  # noqa: F401
 from .summary_invoice import SummaryInvoice  # noqa: F401
 from .summary_invoice import SummaryInvoiceLink  # noqa: F401
 from .summary_invoice_create import SummaryInvoiceCreate  # noqa: F401
